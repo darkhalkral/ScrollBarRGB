b39f03cdc59575a765ed176ce9fbe1387bd05ae8
darkhalkral <84571980+darkhalkral@users.noreply.github.com>
2023-05-25 01:16:56 +0300

    Add files via upload

---
+++ ders3.1/ders3.1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ders3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(27, 27);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(176, 49);
+            this.hScrollBar1.TabIndex = 0;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(27, 90);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(176, 47);
+            this.hScrollBar2.TabIndex = 1;
+            this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.Location = new System.Drawing.Point(27, 152);
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(176, 46);
+            this.hScrollBar3.TabIndex = 2;
+            this.hScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar3_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(249, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 81);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(430, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(430, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(430, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 363);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.hScrollBar3);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ ders3.1/ders3.1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders3._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hScrollBar1.Minimum = 0;
+            hScrollBar1.Maximum = 255;
+            hScrollBar1.LargeChange = 1;
+            hScrollBar1.Value = 1;
+
+            hScrollBar2.Minimum = 0;
+            hScrollBar2.Maximum = 255;
+            hScrollBar2.LargeChange = 1;
+            hScrollBar2.Value = 1;
+
+            hScrollBar3.Minimum = 0;
+            hScrollBar3.Maximum = 255;
+            hScrollBar3.LargeChange = 1;
+            hScrollBar3.Value = 1;
+        }
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            int toplam = 0;
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(hScrollBar1.Value, hScrollBar2.Value, hScrollBar3.Value);
+            toplam= hScrollBar1.Value+hScrollBar2.Value+hScrollBar3.Value;
+            textBox1.Text = (hScrollBar1.Value * 100 / toplam).ToString();
+            textBox2.Text = (hScrollBar2.Value * 100 / toplam).ToString();
+            textBox3.Text = (hScrollBar3.Value * 100 / toplam).ToString();
+        }
+        private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            int toplam = 0;
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(hScrollBar1.Value, hScrollBar2.Value, hScrollBar3.Value);
+            toplam = hScrollBar1.Value + hScrollBar2.Value + hScrollBar3.Value;
+            textBox1.Text = (hScrollBar1.Value * 100 / toplam).ToString();
+            textBox2.Text = (hScrollBar2.Value * 100 / toplam).ToString();
+            textBox3.Text = (hScrollBar3.Value * 100 / toplam).ToString();
+        }
+        private void hScrollBar3_Scroll(object sender, ScrollEventArgs e)
+        {
+            int toplam = 0;
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(hScrollBar1.Value, hScrollBar2.Value, hScrollBar3.Value);
+            toplam = hScrollBar1.Value + hScrollBar2.Value + hScrollBar3.Value;
+            textBox1.Text = (hScrollBar1.Value * 100 / toplam).ToString();
+            textBox2.Text = (hScrollBar2.Value * 100 / toplam).ToString();
+            textBox3.Text = (hScrollBar3.Value * 100 / toplam).ToString();
+        }
+        // Form üzerine 3 adet hscrollbar bir adet texbox form load edildekten sonra 1. horizantal red 2. green 3.blue
+        // 3.ü aynı anda hareket ettirildiğinde rgb renk görüntülensin
+        //RGB Renkleri pictureboxta görüntülerken aynı zamanda textbox1 2 ve 3te her rengin %kaçlık değeri aldığı gözüksün
+    }
+}
